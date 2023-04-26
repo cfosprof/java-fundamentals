2186cbd373f15ed07e156172b39acf13dd605240
Christopher Foster <cfosprof@gmail.com>
2023-04-26 14:04:08 -0700

    Adding comments to App.

--- quotes/app/src/test/java/quotes/AppTest.java
+++ quotes/app/src/test/java/quotes/AppTest.java
@@ -10,28 +10,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
+    // Test case for checking if the getRandomQuote method returns a non-null quote
     @Test
     public void testGetRandomQuote() {
+        // Create a new QuoteService instance
         QuoteService quoteService = new QuoteService();
+        // Get a random quote
         Quote randomQuote = quoteService.getRandomQuote();
+        // Check if the random quote is not null
         assertNotNull(randomQuote, "Random quote should not be null");
     }
 
+    // Test case for checking if the author and text fields in the Quote object are not null and not empty
     @Test
     public void testQuoteFields() {
+        // Create a new QuoteService instance
         QuoteService quoteService = new QuoteService();
+        // Get a random quote
         Quote randomQuote = quoteService.getRandomQuote();
+        // Check if the author field is not null and not empty
         assertNotNull(randomQuote.getAuthor(), "Quote author should not be null");
         assertFalse(randomQuote.getAuthor().isEmpty(), "Quote author should not be empty");
+        // Check if the text field is not null and not empty
         assertNotNull(randomQuote.getText(), "Quote text should not be null");
         assertFalse(randomQuote.getText().isEmpty(), "Quote text should not be empty");
     }
 
+    // Test case for checking if the quotes list in the QuoteService class is populated
     @Test
     public void testQuotesListIsPopulated() {
+        // Create a new QuoteService instance
         QuoteService quoteService = new QuoteService();
+        // Get the list of quotes
         List<Quote> quotes = quoteService.getQuotes();
+        // Check if the quotes list is not null
         assertNotNull(quotes, "Quotes list should not be null");
+        // Check if the quotes list is not empty
         assertFalse(quotes.isEmpty(), "Quotes list should not be empty");
     }
 }
